fcad220f5ab7f10431ca7a45f5f65c9d9f8fe0ea
Fidalgo <fidalgo@Fidalgos-MacBook-Pro.local>
2019-07-04 11:07:05 +0100

    Section 3 done

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,4 +1,9 @@
-const fs = require('fs');
+import fs from 'fs';
+import http from 'http';
+import url from 'url';
+import slugify from 'slugify';
+
+import replaceTemplate from './modules/replaceTemplate';
 
 // // Blocking syncrohnous
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
@@ -8,17 +13,80 @@ const fs = require('fs');
 // const textOut = `This is what ww know about avocado: ${textIn}\nCreated on ${Date.now()}`;
 // fs.writeFileSync('./txt/output.txt', textOut);
 
-// Async  - Non-blocking reading file
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  if (err) return console.log('ERROR!');
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log({ data2 });
-    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console.log({ data3 });
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-        console.log('File written 😎');
-      });
+// // Async  - Non-blocking reading file
+// fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//   if (err) return console.log('ERROR!');
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log({ data2 });
+//     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+//       console.log({ data3 });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+//         console.log('File written 😎');
+//       });
+//     });
+//   });
+// });
+// console.log('Will read file');
+
+// /////////////////////////////
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(item => slugify(item.productName, { lower: true }));
+console.log(slugs);
+
+const server = http.createServer((req, res) => {
+  const { query, pathname } = url.parse(req.url, true);
+
+  // OVERVIEW PAGE
+  if (pathname === '/' || pathname === '/overview') {
+    const cardsHtml = dataObj
+      .map(card => replaceTemplate(tempCard, card))
+      .join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml);
+
+    res.end(output);
+  }
+
+  // PRODUCT PAGE
+  else if (pathname === '/product') {
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
+  }
+
+  // API
+  else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
     });
-  });
+    res.end(data);
+  }
+
+  // NOT FOUND
+  else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'hello world',
+    });
+    res.end('<h1>Page not found</h1>');
+  }
 });
-console.log('Will read file');
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listen to requests on port 8000');
+});
